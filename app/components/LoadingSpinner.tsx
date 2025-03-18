c9cbb77b75c203fcf5a25a51f5d3f3c85301a522
NikolaiPetrov <nipetrov@samokat.ru>
2025-03-18 18:08:17 +0300

    refactor: удалена React версия, оставлена только статическая версия для GitHub Pages

--- app/components/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import styles from './LoadingSpinner.module.css';
-
-export function LoadingSpinner() {
-  return (
-    <div className={styles.spinnerContainer}>
-      <div className={styles.spinner}></div>
-    </div>
-  );
-} 
